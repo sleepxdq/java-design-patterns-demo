ebcd08ea538b896973a609bac5a42cd57e71992a
xudongqiang <xu_dongqiang@foxmail.com>
2018-08-28 17:13:04 +0800

    添加readme

--- factory-method/src/main/java/com/xdq/CardFactory.java
+++ factory-method/src/main/java/com/xdq/CardFactory.java
@@ -3,9 +3,15 @@ package com.xdq;
 /**
  * Author: 徐东强
  * Date: 2018/8/25 下午8:24
- * Description:
+ * Description: 工厂接口
  **/
 public interface CardFactory {
 
-    public Card createCard(CardType cardType) throws Exception;
+    /**
+     * 创建对象
+     * @param cardType
+     * @return
+     * @throws Exception
+     */
+    Card createCard(CardType cardType) throws Exception;
 }
--- factory-method/src/main/java/com/xdq/CardFactoryImpl.java
+++ factory-method/src/main/java/com/xdq/CardFactoryImpl.java
@@ -6,14 +6,19 @@ import java.util.Map;
 /**
  * Author: 徐东强
  * Date: 2018/8/25 下午8:48
- * Description:
+ * Description: 工厂的实现类
  **/
 public class CardFactoryImpl implements CardFactory {
 
     private static Map<CardType,Card> map = new HashMap<CardType, Card>();
 
+    /**
+     * 根据type创建对于的对象
+     * @param cardType
+     * @return
+     * @throws Exception
+     */
     public Card createCard(CardType cardType) throws Exception {
-        Card card = null;
         if (!map.containsKey(cardType)){
             throw new Exception("无此类型的card");
         }
